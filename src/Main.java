061922e47d010fea451f523fabe0f9c2e402ccfc
Jocinjo <josie.knipping@gmail.com>
2023-05-15 13:19:04 +0200

    meer runnable files

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ import java.sql.SQLOutput;
 //check of hij het nog doet
 public class Main{
     public static void main(String[] args) {
-        introductieClass Josie = new introductieClass( "Josie");
-        introductieClass Josie = new introductieClass( "Tommie");
+        introductieClass Josie = new introductieClass( "Josie\n");
+        introductieClass Tommie = new introductieClass( "Tommie");
     }
 }
 
