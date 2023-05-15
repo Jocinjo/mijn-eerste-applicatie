098fa9702c39c3d77c929a978d4677b3cf1f8729
Jocinjo <josie.knipping@gmail.com>
2023-05-15 13:53:02 +0200

    meer runnable files

--- src/introductieClass.java
+++ src/introductieClass.java
@@ -1,9 +1,8 @@
 public class introductieClass {
     public introductieClass(String naam) {
-//        String naam = "Josie";
-        System.out.print(naam);
-        System.out.print("\nHallo ");
-        System.out.print(naam + " welkom!\n");
+        System.out.println(naam);
+        System.out.print("Hallo ");
+        System.out.println(naam + " welkom!");
 
 
     }
