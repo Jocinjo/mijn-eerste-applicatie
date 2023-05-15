7d699a8478ba1765a4b5ecf2ad09e24e871ec4d7
Jocinjo <josie.knipping@gmail.com>
2023-05-15 15:46:18 +0200

    getallen rader maken. stap 1 doet het waarschijnlijk.

---
+++ src/getal_rader.java
@@ -0,0 +1,12 @@
+/*
+stap 1: laat het programma een getal tussen 1 en 1000 kiezen
+stap 2: vraag gebruiker om input met getal.
+stap 3: geef feedback over of heyt getal hoger of later of goed.
+stap 4:zorg dat het programma 10 beurten geeft voor het raden. als er na 10 beurten nog niet het juiste getal is geraden laat dan hett juiste nummer zien.
+optioneel stap 5: zorg dat hij opniuw kan spelen zonder dat het programma stopt.
+ */
+/*
+ */
+public class getal_rader {
+    int randomNum = (int)(Math.random() * 1001);  // 0 to 1000
+}
