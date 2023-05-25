00d2d8d1597c208b826fc546e36e98a59d238a8c
Jocinjo <josie.knipping@gmail.com>
2023-05-25 14:11:42 +0200

    weer aan de bank werken

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,12 @@ import java.sql.SQLOutput;
 //check of hij het nog doet
 public class Main{
     public static void main(String[] args) {
-//       Bank bank = new Bank();
-//       bank.createAccount("NL01ABCD1234567890", 100.00);
-//       bank.createAccount("NL02EFGH1234567890", 500.00);
-//       bank.deposit("NL01ABCD1234567890", 250.00);
-//       bank.withdraw("NL02EFGH1234567890", 200.00);
-//       bank.getAccount("NL01ABCD1234567890");
-//        Schaakbord s = new Schaakbord();
-//                s.make();
-        Lamp een = new Lamp("aan");
-        Lamp twee = new Lamp("uit");
-        Lamp drie = new Lamp("wat");
-        System.out.println(twee.isAan);
-        drie.status = "uit";
-        System.out.println(drie.status);
-        System.out.println(drie.isAan);
-        een.status = "aan";
-        System.out.println(een.status);
-        een.aan();
-        een.status = "aan";
-        een.aan();
-        een.status = "uit";
-        een.status = "uit";
-        een.aan();
-        een.uit();
-        een.uit();
+       Bank bank = new Bank();
+       bank.createAccount("NL01ABCD1234567890", 100.00);
+       bank.createAccount("NL02EFGH1234567890", 500.00);
+       bank.deposit("NL01ABCD1234567890", 250.00);
+       bank.withdraw("NL02EFGH1234567890", 200.00);
+       bank.getAccount("NL01ABCD1234567890");
 
 
     }
