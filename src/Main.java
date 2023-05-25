12d54bfb970b930f87bc33a569a2f0f0eeb38968
Jocinjo <josie.knipping@gmail.com>
2023-05-25 15:42:34 +0200

    + en - werkt bij de banken.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-import java.sql.SQLOutput;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-//check of hij het nog doet
+//check of hij het nog doet.
 public class Main{
     public static void main(String[] args) {
        Bank bank = new Bank();
