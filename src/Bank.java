cb288ab2f496c533e90ef1ccf09adaeb1dfcf61a
Jocinjo <josie.knipping@gmail.com>
2023-05-30 15:54:43 +0200

    uit bank alle nutteloze dingen eruit gehaald.

--- src/Bank.java
+++ src/Bank.java
@@ -1,14 +1,10 @@
 //ik heb nu scanners gemaakt maar dat moet eigenlijk niet. de rest werkt waarschijnlijk al
 
-import java.security.KeyStore;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 
     public class Bank {
-        //    Scanner input = new Scanner(System.in);
+
         String accountNumber;
-        //    int beginsaldo = input.nextInt();
         double balance;
         double amount;
         HashMap<String, BankAccount> Bankaccounts = new HashMap<String, BankAccount>();
@@ -20,12 +16,10 @@ import java.util.Scanner;
             this.Bankaccounts = Bankaccounts;
         }
 
-        // onderstaande dadelijk nog verder aanvu;len maar eerst alle methodes even schrijven voor de zekerheid.
+        // onderstaande dadelijk nog verder aanvullen maar eerst alle methodes even schrijven voor de zekerheid.
         public void createAccount(String accountNumber, double bedrag) {
             BankAccount tijdelijk = new BankAccount(accountNumber);
-            // aanmaken van een account
             System.out.print("jouw rekeningnummer is: " + accountNumber + "en je beginbalance is: ");
-//            System.out.println(balance);
             Bankaccounts.put(accountNumber, tijdelijk);
 
         }
@@ -57,7 +51,6 @@ import java.util.Scanner;
         }
 
         public Double getBalance(String accountNumber) {
-//            System.out.println(Bankaccounts.get(accountNumber));
             return balance;
 
         }
@@ -69,8 +62,6 @@ import java.util.Scanner;
 
         public String toString() {
             String zoekobject = accountNumber;
-//            double x = Bankaccounts.get(zoekobject);
-//            return "Jouw rekeningnummer is " + accountNumber + "and your balance is " + (Double.toString(x));
             return Bankaccounts.toString();
         }
         public void printString() {
