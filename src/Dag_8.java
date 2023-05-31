3d2a5f9d08896911e0d839b20b930418f5b2d730
Jocinjo <josie.knipping@gmail.com>
2023-05-31 17:08:47 +0200

    dag 8 met behulp van het schaakbord lukt nog niet. het schaakbord wil wel maar iets toevoegen aan de tekst voor een bepaalde conditie lukt me nog niet.

--- src/Dag_8.java
+++ src/Dag_8.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 /*
 ik kan geen kleuren geven voor nu omdat het niet kan in intellij. dus ik ga er X'en bij zetten als het kan.
  */
@@ -11,6 +12,10 @@ public class Dag_8 {
     public int verticaal;
     public int aantalh;
     public int aantalv;
+    public int left = aantalh - 1;
+    public int right = aantalh + 1;
+    public int up = aantalv - 1;
+    public int down = aantalv + 1;
 
 
     public Dag_8() {
@@ -19,22 +24,35 @@ public class Dag_8 {
         this.aantalh = aantalh;
         this.aantalv = aantalv;
         this.getallen = getallen;
+        this.left = left;
+        this.right = right;
+        this.up = up;
+        this.down = down;
+
 
     }
-//misschien 4 verschillende variabelen maken dus links recht boven onver en dan die aan elkaar plakken of zo.
+
+    //misschien 4 verschillende variabelen maken dus links recht boven onver en dan die aan elkaar plakken of zo.
     public void make() {
-        for (aantalh = 0; aantalh < getallen; aantalh++) {
-            for (aantalv = 0; aantalv < getallen; aantalv++) {
-                System.out.print("[" + aantalh + "," + aantalv + "] ");
-                //misschien een extra forloop om te printen welke cijfers ++ en -- zijn
-                if(aantalh -= 1 getallen) {
-                    System.out.println("[" + aantalh + "," + aantalv + "]");
+        for (aantalh = 0; aantalh <= getallen; aantalh++) {
+            for (aantalv = 0; aantalv <= getallen; aantalv++) {
+                if (aantalh == left) {
+                    System.out.println("[" + aantalh + "x," + aantalv + "]");
+                } else {
+                    System.out.print("[" + aantalh + "," + aantalv + "] ");
                 }
-                System.out.println();
-                System.out.println((aantalh-- + "x") + (aantalh + "x") + (aantalh++ + "x") + " " + (aantalv-- + "x") + (aantalv + "x") + (aantalv++ + "x"));
+
+//                misschien een extra forloop om te printen welke cijfers ++ en -- zijn
+
             }
+            System.out.println();
         }
     }
 }
 
 
+// matches()
+//contentEquals()
+//contains
+//replace() --> deze proberen.
+//split()
