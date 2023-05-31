5fc80424c9d63c5f52aab8152b12cfd351b37bdc
Jocinjo <josie.knipping@gmail.com>
2023-05-31 17:54:18 +0200

    dag 9 doet het!

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,7 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
 
-//       Dag_7 persoon = new Dag_7();
-//       persoon.totalPoints();
-//       persoon.totalPoints();
-//       persoon.totalPoints();
 
-//       Dag_8 bord = new Dag_8();
-//       bord.make();
-
-//        Scanner input = new Scanner(System.in);
-//        System.out.print("Enter the number of coordinates: ");
-//        int getal = input.nextInt();
-//        Dag_8 dag8 = new Dag_8(getal);
-//        dag8.make();
-
-//        hij laat nog wel alle 4ren onder elkaar zien. kijken of ik dat nog kan fixen.
         Dag_9 tijd = new Dag_9();
         tijd.clock();
 
