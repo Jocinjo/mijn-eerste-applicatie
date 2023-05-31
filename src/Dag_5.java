dddd11c4b1467f3e4db11211d626ad19a4b53460
Jocinjo <josie.knipping@gmail.com>
2023-05-31 11:22:41 +0200

    deel 2 van dag 5 gedaan. alle ints moesten een double zijn omdat hij anders de verkeerde cijfers geeft.

--- src/Dag_5.java
+++ src/Dag_5.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class Dag_5 {
-//    deel 1 van dag 5 zit in dag 4 verwerkt omdat hij daarop voortbouwt.
+   /*  deel 1 van dag 5 zit in dag 4 verwerkt omdat hij daarop voortbouwt.
+    deel 2 van de dag 5 zal hier staan
+
+    input voor base en heigt
+
+    en uiteindelijk area = base x height / 2
+    */
+
+
+
+    public Dag_5() {
+
+
+    }
+
+    public void berekenDeArea() {
+        Scanner input = new Scanner(System.in);
+        double base = input.nextDouble();
+        double height = input.nextDouble();
+
+        double area = (base*height)/2;
+        System.out.print("the area is ");
+        System.out.println(area);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,9 @@
 //check of hij het nog steeds doet.
 public class Main{
     public static void main(String[] args) {
-   Dag_4 printen = new Dag_4();
-   printen.probeer();
-   printen.aBoolean = false;
-   printen.aChar = 'D';
-   printen.aDouble = 5.6666;
-   printen.by = 9;
-   printen.aFloat = 7F;
-   printen.anInt = 500;
-   printen.aLong = 555L;
-   printen.aString = "andere variabelen";
-   printen.sho = -30000;
-   printen.probeer();
+       Dag_5 veluwe = new Dag_5();
+       veluwe.berekenDeArea();
+
 
 
 
