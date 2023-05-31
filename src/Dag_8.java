5e9651f3de60a4a7495736b5149042394d0e2699
Jocinjo <josie.knipping@gmail.com>
2023-05-31 17:26:28 +0200

    met behulp van chat gtp doet hij nu de lagere cijfers markeren. maar wel in een kruis. dus nog niet 1 bij elk vierkantje eromheen.

--- src/Dag_8.java
+++ src/Dag_8.java
@@ -1,58 +1,91 @@
+//import java.util.Scanner;
+//
+///*
+//ik kan geen kleuren geven voor nu omdat het niet kan in intellij. dus ik ga er X'en bij zetten als het kan.
+// */
+//public class Dag_8 {
+//
+//    Scanner input = new Scanner(System.in);
+//    public String getal = input.next();
+//    public int getallen = Integer.parseInt(getal);
+//    public int horizontaal = 0;
+//    public int verticaal;
+//    public int aantalh;
+//    public int aantalv;
+//    public int left = aantalh - 1;
+//    public int right = aantalh + 1;
+//    public int up = aantalv - 1;
+//    public int down = aantalv + 1;
+//
+//
+//    public Dag_8() {
+//        this.horizontaal = horizontaal;
+//        this.verticaal = verticaal;
+//        this.aantalh = aantalh;
+//        this.aantalv = aantalv;
+//        this.getallen = getallen;
+//        this.left = left;
+//        this.right = right;
+//        this.up = up;
+//        this.down = down;
+//
+//
+//    }
+//
+//    //misschien 4 verschillende variabelen maken dus links recht boven onver en dan die aan elkaar plakken of zo.
+//    public void make() {
+//        for (aantalh = 0; aantalh <= getallen; aantalh++) {
+//            for (aantalv = 0; aantalv <= getallen; aantalv++) {
+//                if (aantalh == left) {
+//                    System.out.println("[" + aantalh + "x," + aantalv + "]");
+//                } else {
+//                    System.out.print("[" + aantalh + "," + aantalv + "] ");
+//                }
+//
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+//
+//
+//// matches()
+////contentEquals()
+////contains
+////replace() --> deze proberen.
+////split()
+
 import java.util.Scanner;
 
-/*
-ik kan geen kleuren geven voor nu omdat het niet kan in intellij. dus ik ga er X'en bij zetten als het kan.
- */
 public class Dag_8 {
 
-    Scanner input = new Scanner(System.in);
-    public String getal = input.next();
-    public int getallen = Integer.parseInt(getal);
-    public int horizontaal = 0;
-    public int verticaal;
-    public int aantalh;
-    public int aantalv;
-    public int left = aantalh - 1;
-    public int right = aantalh + 1;
-    public int up = aantalv - 1;
-    public int down = aantalv + 1;
+    private int getallen;
+    private int left;
+    private int right;
+    private int up;
+    private int down;
 
-
-    public Dag_8() {
-        this.horizontaal = horizontaal;
-        this.verticaal = verticaal;
-        this.aantalh = aantalh;
-        this.aantalv = aantalv;
+    public Dag_8(int getallen) {
         this.getallen = getallen;
-        this.left = left;
-        this.right = right;
-        this.up = up;
-        this.down = down;
-
-
+        this.left = getallen - 1;
+        this.right = getallen + 1;
+        this.up = getallen - 1;
+        this.down = getallen + 1;
     }
 
-    //misschien 4 verschillende variabelen maken dus links recht boven onver en dan die aan elkaar plakken of zo.
     public void make() {
-        for (aantalh = 0; aantalh <= getallen; aantalh++) {
-            for (aantalv = 0; aantalv <= getallen; aantalv++) {
-                if (aantalh == left) {
-                    System.out.println("[" + aantalh + "x," + aantalv + "]");
+        for (int aantalh = 0; aantalh <= getallen; aantalh++) {
+            for (int aantalv = 0; aantalv <= getallen; aantalv++) {
+                if (aantalh == left || aantalv == up) {
+                    System.out.print("[" + aantalh + "x," + aantalv + "] ");
                 } else {
-                    System.out.print("[" + aantalh + "," + aantalv + "] ");
+                    System.out.print("[" + aantalh + ",," + aantalv + "] ");
                 }
-
-//                misschien een extra forloop om te printen welke cijfers ++ en -- zijn
-
             }
             System.out.println();
         }
     }
-}
 
 
-// matches()
-//contentEquals()
-//contains
-//replace() --> deze proberen.
-//split()
+    }
+
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -10,9 +11,16 @@ public class Main{
 //       persoon.totalPoints();
 //       persoon.totalPoints();
 
-       Dag_8 bord = new Dag_8();
-       bord.make();
+//       Dag_8 bord = new Dag_8();
+//       bord.make();
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of coordinates: ");
+        int getal = input.nextInt();
+        Dag_8 dag8 = new Dag_8(getal);
+        dag8.make();
+
+//        hij laat nog wel alle 4ren onder elkaar zien. kijken of ik dat nog kan fixen.
 
 
 
