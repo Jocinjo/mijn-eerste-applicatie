785f5dca05c8bfe561d7b32629112ecf2b570f00
Jocinjo <josie.knipping@gmail.com>
2023-05-31 11:02:38 +0200

    values veranderd vanuit de mainklasse voor dag 5. de values staan in dag 4.

---
+++ src/Dag_5.java
@@ -0,0 +1,3 @@
+public class Dag_5 {
+//    deel 1 van dag 5 zit in dag 4 verwerkt omdat hij daarop voortbouwt.
+}
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,16 @@ public class Main{
     public static void main(String[] args) {
    Dag_4 printen = new Dag_4();
    printen.probeer();
+   printen.aBoolean = false;
+   printen.aChar = 'D';
+   printen.aDouble = 5.6666;
+   printen.by = 9;
+   printen.aFloat = 7F;
+   printen.anInt = 500;
+   printen.aLong = 555L;
+   printen.aString = "andere variabelen";
+   printen.sho = -30000;
+   printen.probeer();
 
 
 
