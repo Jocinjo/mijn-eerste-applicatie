926a1b765f27b655592d33157bc898bf2adf319c
Jocinjo <josie.knipping@gmail.com>
2023-06-02 15:31:17 +0200

    dag 14 nog niet gemaakt.

---
+++ src/Dag_14.java
@@ -0,0 +1,11 @@
+/*
+maak een array van 5
+vraag voor 5 nummers bij de array en stop deze erin met put.
+maak een tweede array van 5 elementen
+copy the array met een forloop in de tweede array
+laat beide arrays zien met print.
+ */
+public class Dag_14 {
+    public Dag_14() {}
+
+}
