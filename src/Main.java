c4dc89a4afc2f93d14a5fa43057753bfb613b8bc
Jocinjo <josie.knipping@gmail.com>
2023-06-08 11:04:30 +0200

    try en catch in de main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@
 // then press Enter. You can now see whitespace characters in your code.
 //check of hij het nog steeds doet.
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -13,9 +15,18 @@ public class Main {
 
 
 
-        Dag_15 freach = new Dag_15();
-        freach.min();
+//        Dag_15 freach = new Dag_15();
+//        freach.min();
 
+        try {
+            FileInputStream fis = new FileInputStream("test.txt");
+        }catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("deze laat hij alleen zien als de eerste fout een out of bounds is");
+        }finally {
+            System.out.println("Try exception is done");
+        }
 
     }
 }
