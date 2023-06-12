a8518f9dcfcbf3e34574b3ab25d3fea3a871d1b1
Jocinjo <josie.knipping@gmail.com>
2023-06-12 12:41:51 +0200

    bytes met byte operators

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Random;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
@@ -18,9 +18,30 @@ public class Main{
 //        Dag_18 attack = new Dag_18();
 //        attack.guessPosition();
 
-        int i = 128;
-        byte b = (byte) i;
-        System.out.println(b); //blijkbaar kun je niet van int naar byte typecasten omdat je dan -128 krijgt.
+//        int i = 128;
+//        byte b = (byte) i;
+//        System.out.println(b); //blijkbaar kun je niet van int naar byte typecasten omdat je dan -128 krijgt.
+
+        byte trein = 120; // 1111000
+        System.out.println(isLichtAan(trein));
+        System.out.println(rijRichting(trein));
+        System.out.println(trein);
     }
+
+    public static boolean isLichtAan(int trein) {
+        return (trein >> 3 & 1) == 1;
+    }
+
+    public static boolean rijRichting(int trein) {
+        return (trein >> 3 & 1) == 1;
+    }
+
+    public static boolean snelheid(int trein) {
+        //moet 67 uitkomen dus 64 2 1
+        return (trein >> 4 & 11) == 1;
+    }
+
+
 }
+
 
