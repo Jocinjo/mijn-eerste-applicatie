9b879337041006513f9c9698ca0338903a0a7c4c
Jocinjo <josie.knipping@gmail.com>
2023-06-13 12:21:57 +0200

    try en catch in de scanners werkt

--- src/DifferentScanners.java
+++ src/DifferentScanners.java
@@ -9,20 +9,29 @@ public class DifferentScanners {
     static Scanner input = new Scanner(System.in);
 
     static int askForNumber(String text) { //text is what will be asked from the console
+        int number = Integer.MIN_VALUE;
         System.out.println(text);
         String scanned = input.next();
-        int number = Integer.parseInt(scanned);
+        try {
+            return Integer.parseInt(scanned);
+        } catch (NumberFormatException e) {
+            System.out.println(e + "\nhet is geen cijfer");
+            askForNumber(text);
+        }
         return number;
     }
 
     static int askForNumberInRange(String text, int min, int max) { //this one will give a range to work with
-        int number;
+        int number = min -1;
         do {
             System.out.println(text);
             String scanned = input.next();
-            number = Integer.parseInt(scanned);
-        }
-        while (number < min || number > max);
+            try {
+                number = Integer.parseInt(scanned);
+            } catch (NumberFormatException e) {
+                System.out.println(e + "\ngeef alsjebieft een nummer en geen andere karakters.");
+            }
+        } while (number < min || number > max);
         return number;
     }
 }
---
+++ src/TryCatch.java
@@ -0,0 +1,17 @@
+public class TryCatch extends DifferentScanners{
+    public static void main(String[] args) {
+        try {
+            int nummer = 5 / 1;
+
+        } catch (ArithmeticException e) {
+            System.out.println(e + "\nDit is niet goed!");
+        } finally {
+            System.out.println("zo lang het maar geen 0 is. deze print altijd");
+        }
+        int getal = askForNumberInRange("geef een nummer", -100, 1000);
+        System.out.println(getal);
+
+        int cijfer = askForNumber("geef een nummer nog een keer");
+        System.out.println(cijfer);
+    }
+}
